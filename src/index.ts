70330e4031013019cc969311c4e22f47039dccb2
Tim Tran <tim@Tims-MacBook-Pro.local>
2023-04-13 23:44:18 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import {default as Dialog} from './Dialog';
 import {default as Dropdown} from './Dropdown';
 import {default as DropdownItem} from './DropdownItem';
 import {default as Switch} from './Switch';
+import {default as Checkbox} from './Checkbox';
 
 export {
     Input,
@@ -25,5 +26,6 @@ export {
     Dialog,
     Dropdown,
     DropdownItem,
-    Switch
+    Switch,
+    Checkbox
 };
